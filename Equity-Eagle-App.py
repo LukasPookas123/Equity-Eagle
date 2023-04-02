75597843e1008fc4e620a70194961132190f3a05
oculur <viner.louis@gmail.com>
2023-04-02 09:43:35 +0100

    description

--- Equity-Eagle-App.py
+++ Equity-Eagle-App.py
@@ -6,6 +6,14 @@ import yfinance as yf
 import datetime as dt
 from plotly.subplots import make_subplots
 from numerize import numerize
+import os
+import pandas as pd
+import datetime as dt
+import os
+from bs4 import BeautifulSoup
+from textblob import TextBlob
+import eikon as ek
+import numpy as np
 
 
 def equity_graph(df):
@@ -258,8 +266,8 @@ def peer_PE_bar():
         return fig
 
 def volatility(ticker):
-    # dataPath = os.path.abspath(os.path.join(os.path.dirname((os.path.curdir.__dir__()[0])), '..', 'Data'))
-    # fundDF = pd.read_csv(dataPath+'\\{}\\Fundamentals.csv'.format(ticker))
+    dataPath = os.path.abspath(os.path.join(os.path.dirname((os.path.curdir.__dir__()[0])), '..', 'Data'))
+    fundDF = pd.read_csv(dataPath+'\\{}\\Fundamentals.csv'.format(ticker))
     pDF = pd.read_csv(dataPath+'\\{}\\Peers.csv'.format(ticker))
     volAvg = pDF['TR.VOLATILITY260D/100'].mean()
     volAsset = fundDF['TR.VOLATILITY260D/100'][0]
@@ -314,11 +322,12 @@ def volatility(ticker):
 # dataPath = os.path.abspath(os.path.join(os.path.dirname((os.path.curdir.__dir__()[0])), '..', 'Data'))
 # figs = volatility('2222.SE',pd.read_csv(dataPath+'\\2222.SE\\Fundamentals.csv'),pd.read_csv(dataPath+'\\2222.SE\\Peers.csv'))
 # figs.show()
-
+test = "Equity-Eagle\AllStockNames.csv"
+dataPath = os.path.abspath(os.path.join(os.path.dirname((os.path.curdir.__dir__()[0])), '..', 'Equity-Eagle'))
 
 @st.cache()
 def get_stock_names():
-    df = pd.read_csv('/Users/faizrahman/Desktop/Equity-Eagle/AllStockNames.csv')
+    df = pd.read_csv(test)
     company_names = list(df['Company Common Name'])
     return company_names
 
@@ -332,12 +341,103 @@ st.title(ticker)
 '''
 Add in the data gathering code under here:
 '''
+dataPath = os.path.abspath(os.path.join(os.path.dirname((os.path.curdir.__dir__()[0])), '..', 'Equity-Eagle'))
+df = pd.read_csv("C:\\Users\\paulv\\Documents\\Uni\\Hackathon\\Equity-Eagle\\AllStockNames.csv")
+name = ticker
+ticker = df.loc[df['Company Common Name'] == ticker]['Updated at 18:21:34'][0]
+
+
+ek.set_app_key('04c0e3f661bd49348d69c3aabedb8c0108cfd1e2')
+#start and end date; it is in format yyyy-mm-dd
+now = dt.datetime.now()
+start = now - dt.timedelta(days = 365*25)
+
+#initialising demo of generating timeseries for list of stock tickers
+dfs = list()
+myDir = ("Data")
+dataPath = os.path.abspath(os.path.join(os.path.dirname((os.path.curdir.__dir__()[0])), '..', 'Data')) #goes up a dir to equity eagle then into Data dir
+#get all tickers currently downloaded
+
+#need to check if ticker user inputted in top 5000? design decision tbd
+    #check if ticker downloaded
+dataPath = "Equity-Eagle\\Data\\{}".format(ticker)
+temp = os.path.exists(dataPath)
+if os.path.isdir(dataPath):
+    #get news df
+    df = ek.get_news_headlines('PresetTopic:[Significant News: All] AND R:{} AND Language:LEN'.format(ticker), count=5)
+    df['HTML'] = df['storyId'].apply(ek.get_news_story) #create col with article html
+    #get sentiments and objectivity
+    df['Polarity'] = np.nan
+    df['Subjectivity'] = np.nan
+    df['Score'] = np.nan
+
+    for idx, storyId in enumerate(df['storyId'].values):
+        newsText = ek.get_news_story(storyId) #get the news story
+        if newsText:
+            soup = BeautifulSoup(newsText,"lxml") #create a BeautifulSoup object from our HTML news article
+            sentA = TextBlob(soup.get_text()) #pass the text only article to TextBlob to anaylse
+            df['Polarity'].iloc[idx] = sentA.sentiment.polarity #write sentiment polarity back to df
+            df['Subjectivity'].iloc[idx] = sentA.sentiment.subjectivity #write sentiment subjectivity score back to df
+            if sentA.sentiment.polarity >= 0.05: # attribute bucket to sentiment polartiy
+                score = 'positive'
+            elif  -.05 < sentA.sentiment.polarity < 0.05:
+                score = 'neutral'
+            else:
+                score = 'negative'
+            df['Score'].iloc[idx] = score #write score back to df
+    
+    #check if news csv exists- superflous????      
+    if os.path.exists(dataPath + "\\News.csv"):
+        #overwrite csv
+        df.to_csv(path_or_buf=dataPath+"\\News.csv",mode='w')
+
+    else:
+        #create csv and write top 5
+        df.to_csv(path_or_buf=dataPath+"\\News.csv",mode='w')
+else:
+    #make dir and add 
+
+    #add timeseries csv
+    df = ek.get_timeseries(ticker,'CLOSE',interval='daily',start_date=start,end_date=now)
+    os.makedirs(dataPath)
+    df.to_csv(path_or_buf = dataPath + "\Prices.csv")
+
+    #add peers csv
+    df,err = ek.get_data('PEERS("{}")'.format(ticker),
+    [
+    'TR.F.EVToEBIT','TR.F.EVToEBITDA','TR.EVToSales(1D)','TR.FwdPtoEPSSmartEst','TR.PriceToBVPerShare',
+    'TR.PriceToCFPerShare','TR.PriceClose/TR.FreeOperatingCashFlowperShareAvgDilutedSharesOut','TR.Volatility260D/100',
+    'TR.ReturnonAvgTotEqtyPctNetIncomeBeforeExtraItems/100', 'TR.ROATotalAssetsPercent/100','TR.GrossMargin/100',
+    'TR.NetIncome/TR.Revenue','TR.LTDebtToTtlEqtyPct/100','TR.LTDebtToTtlCapitalPct/100','TR.TimesInterestEarned',
+    'PERATIO'
+    ])
+    df['Price / EPS (SmartEstimate ®)'] = pd.to_numeric(df['Price / EPS (SmartEstimate ®)'],errors='coerce') #remove nan string in starMine col
+    df = df.dropna(axis=0,how='any')
+    df.to_csv(path_or_buf = dataPath + "\Peers.csv")
+
+    #add fundamentals csv
+    df,err = ek.get_data(ticker,
+    [
+    'TR.F.EVToEBIT','TR.F.EVToEBITDA','TR.EVToSales(1D)','TR.FwdPtoEPSSmartEst','TR.PriceToBVPerShare',
+    'TR.PriceToCFPerShare','TR.PriceClose/TR.FreeOperatingCashFlowperShareAvgDilutedSharesOut', 'TR.Volatility260D/100',
+    'TR.ReturnonAvgTotEqtyPctNetIncomeBeforeExtraItems/100', 'TR.ROATotalAssetsPercent/100','TR.GrossMargin/100',
+    'TR.NetIncome/TR.Revenue','TR.LTDebtToTtlEqtyPct/100','TR.LTDebtToTtlCapitalPct/100','TR.TimesInterestEarned',
+    'TR.BusinessSummary','TR.BusinessSummary','TR.OrgFoundedYear','TR.CompanyNumEmploy','TR.OrganizationWebsite',
+    'TR.Revenue','TR.CompanyMarketCap','TR.RepNetProfitMean','PERATIO','TR.PriceToSalesPerShare',
+    'TR.F.COGSTot','TR.DividendYield','TR.F.DivPayoutRatioPct/100'
+    ])
+    df['Price / EPS (SmartEstimate ®)'] = pd.to_numeric(df['Price / EPS (SmartEstimate ®)'],errors='coerce') #remove nan string in starMine col
+    df = df.dropna(axis=0,how='any')
+    df.to_csv(path_or_buf = dataPath + "\Fundamentals.csv")
+
+
+
 
 
 st.write('---')
-st.header(f'{ticker} Company Overview')
+st.header(f'{name} Company Overview')
 st.subheader('1.1 Description')
-description = '''Apple Inc. is an American multinational technology company headquartered in Cupertino, California. Apple is the largest technology company by revenue, totaling US$394.3 billion in 2022.[6] As of March 2023, Apple is the world's biggest company by market capitalization.[7] As of June 2022, Apple is the fourth-largest personal computer vendor by unit sales and second-largest mobile phone manufacturer. It is one of the Big Five American information technology companies, alongside Alphabet (known for Google), Amazon, Meta (known for Facebook), and Microsoft.'''
+description = pd.read_csv(dataPath+'\\Fundamentals.csv')['Business Description'][0]
 st.write(description)
 
 #1.2 Stock performance Graph
